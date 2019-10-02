0db4d7903941b8b9aa3ae757e2d5f36cca8d2e9a
msallent <mqsallent@gmail.com>
2019-10-01 21:52:13 -0300

    Fix file having wrong comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 /**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ * Implement Gatsby's Node APIs in this file.
  *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
 // You can delete this file if you're not using it
