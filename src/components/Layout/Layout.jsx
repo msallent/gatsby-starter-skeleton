34ba89ffa322fd82b585a51d5107566ad67d1561
msallent <mqsallent@gmail.com>
2019-10-01 21:53:31 -0300

    Add PropTypes to type children prop

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import GlobalStyle from './styles';
 import SEO from '../SEO/SEO';
@@ -10,5 +11,9 @@ const Layout = ({ children }) => (
     {children}
   </>
 );
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Layout;
