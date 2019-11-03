18d37fa145c588d909e99fda90d2e154378a0660
msallent <mqsallent@gmail.com>
2019-11-02 22:01:30 -0300

    Add Layout title prop

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { HelmetProvider } from 'react-helmet-async';
 
 import SEO from '../SEO';
 
 import GlobalStyle from '../../utils/globalStyle';
 
-const Layout = ({ children }) => (
-  <>
+const Layout = ({ children, title }) => (
+  <HelmetProvider>
     <GlobalStyle />
-    <SEO />
+    <SEO title={title} />
     {children}
-  </>
+  </HelmetProvider>
 );
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  title: PropTypes.string,
+};
+
+Layout.defaultProps = {
+  title: '',
 };
 
 export default Layout;
