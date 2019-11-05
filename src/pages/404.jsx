063258b833a02321f55e9e34c116200a936ccdcd
msallent <mqsallent@gmail.com>
2019-11-04 23:39:38 -0300

    Add a really basic 404 error page

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const NotFound = () => (
+  <Layout title="404: Not Found">
+    <div>Error 404 - Not Found</div>
+  </Layout>
+);
+
+export default NotFound;
