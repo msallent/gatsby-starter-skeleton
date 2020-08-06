9ca10c6d496ba39665392d68b799c64a0e6ae4f1
Matias Sallent <matias.sallent@mediamonks.com>
2020-08-06 13:49:57 -0300

    Set config entry point to use ts-config plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const { generateConfig } = require('gatsby-plugin-ts-config');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Skeleton',
-    description: 'Gatsby starter featuring TypeScript, ESLint, Prettier and more...',
-    keywords: ['gatsby', 'starter', 'typescript', 'eslint', 'prettier', 'layout', 'seo'],
-    imageURI: '/images/social.jpg',
-    siteUrl: 'http://localhost:8000',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-sass',
-      options: {
-        cssLoaderOptions: {
-          localIdentName: isProduction
-            ? '[local]-[hash:base64:5]'
-            : '[name]_[local]-[hash:base64:5]',
-        },
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-svgr',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/assets`,
-        name: 'assets',
-      },
-    },
-    'gatsby-transformer-sharp',
-  ],
-};
+module.exports = generateConfig({
+  configDir: './src/gatsby',
+  tsNode: true,
+});
