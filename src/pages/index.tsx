4dc8cc1a39899eddfefdcbb692e5f5ba3fb6e889
msallent <mqsallent@gmail.com>
2020-10-31 18:52:31 -0300

    Move index content to its corresponding file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import React, { FunctionComponent } from 'react';
-import { App } from '../components/App';
+import { PageProps } from 'gatsby';
+import { ReactComponent as Logo } from '../assets/svg/gatsby.svg';
+import styles from '../style/pages/index.module.scss';
 
-const Index: FunctionComponent = () => <App />;
+const Index: FunctionComponent<PageProps> = () => (
+  <div className={styles.wrapper}>
+    <Logo className={styles.logo} />
+    <p>Delete this file to start your project.</p>
+    <a className={styles.link} href="https://github.com/msallent/gatsby-starter-skeleton">
+      Gatsby Skeleton
+    </a>
+  </div>
+);
 
 export default Index;
