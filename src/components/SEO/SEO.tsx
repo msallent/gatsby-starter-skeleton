a906157675356bf0edf83728aec1ef68c8d74090
Matias Sallent <matias.sallent@mediamonks.com>
2021-01-29 12:41:44 -0300

    Refactor to use HTML elements

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -11,49 +11,18 @@ export const SEO: FunctionComponent<SEOProps> = ({ location }) => {
   const { title, description, keywords, siteUrl, imageUrl, language } = useSiteMetadata();
 
   return (
-    <Helmet
-      defaultTitle={title}
-      titleTemplate={`${title} - %s`}
-      htmlAttributes={{ lang: language }}
-      link={[{ rel: 'icon', type: 'image/ico', href: Favicon }]}
-      meta={[
-        {
-          name: 'description',
-          content: description,
-        },
-        {
-          property: 'keywords',
-          content: keywords.join(', '),
-        },
-        {
-          property: 'og:title',
-          content: title,
-        },
-        {
-          property: 'og:url',
-          content: `${siteUrl}${location.pathname}`,
-        },
-        {
-          property: 'og:description',
-          content: description,
-        },
-        {
-          property: 'og:type',
-          content: 'website',
-        },
-        {
-          property: 'og:image',
-          content: `${siteUrl}${imageUrl}`,
-        },
-        {
-          property: 'og:image:alt',
-          content: title,
-        },
-        {
-          name: 'twitter:card',
-          content: 'summary_large_image',
-        },
-      ]}
-    />
+    <Helmet defaultTitle={title} titleTemplate={`${title} - %s`}>
+      <html lang={language} />
+      <link rel="icon" type="image/ico" href={Favicon} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords.join(', ')} />
+      <meta name="og:title" content={title} />
+      <meta name="og:url" content={`${siteUrl}${location.pathname}`} />
+      <meta name="og:description" content={description} />
+      <meta name="og:type" content="website" />
+      <meta name="og:image" content={`${siteUrl}${imageUrl}`} />
+      <meta name="og:image:alt" content={title} />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Helmet>
   );
 };
