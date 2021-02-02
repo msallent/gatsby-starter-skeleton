4ac82ec489112e936e042d0bb63bcb414fe9a035
msallent <mqsallent@gmail.com>
2021-02-02 01:28:41 -0300

    Add option to have per-page custom metadata

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -3,25 +3,32 @@ import { Helmet } from 'react-helmet';
 import { useSiteMetadata } from '../../hooks/useSiteMetadata';
 import Favicon from '../../assets/favicon.ico';
 
-interface SEOProps {
+export interface SEOProps {
   location: Location;
+  pageMetadata?: {
+    title?: string;
+    description?: string;
+    keywords?: Array<string>;
+    imageUrl?: string;
+    language?: string;
+  };
 }
 
-export const SEO: FunctionComponent<SEOProps> = ({ location }) => {
+export const SEO: FunctionComponent<SEOProps> = ({ location, pageMetadata }) => {
   const { title, description, keywords, siteUrl, imageUrl, language } = useSiteMetadata();
 
   return (
-    <Helmet defaultTitle={title} titleTemplate={`${title} - %s`}>
-      <html lang={language} />
+    <Helmet title={pageMetadata?.title} defaultTitle={title} titleTemplate={`${title} - %s`}>
+      <html lang={pageMetadata?.language || language} />
       <link rel="icon" type="image/ico" href={Favicon} />
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords.join(', ')} />
-      <meta name="og:title" content={title} />
+      <meta name="description" content={pageMetadata?.description || description} />
+      <meta name="keywords" content={(pageMetadata?.keywords || keywords).join(', ')} />
+      <meta name="og:title" content={pageMetadata?.title || title} />
       <meta name="og:url" content={`${siteUrl}${location.pathname}`} />
-      <meta name="og:description" content={description} />
+      <meta name="og:description" content={pageMetadata?.description || description} />
       <meta name="og:type" content="website" />
-      <meta name="og:image" content={`${siteUrl}${imageUrl}`} />
-      <meta name="og:image:alt" content={title} />
+      <meta name="og:image" content={pageMetadata?.imageUrl || `${siteUrl}${imageUrl}`} />
+      <meta name="og:image:alt" content={pageMetadata?.title || title} />
       <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   );
